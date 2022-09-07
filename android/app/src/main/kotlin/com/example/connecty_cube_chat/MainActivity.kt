7bd7b56b7dfd86716b530e403ef2a5002133ec4d
Berkan Sevimli <berkansevimli@192.168.1.59>
2022-09-07 09:09:36 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/connecty_cube_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.connecty_cube_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,22 @@
+import 'package:connectycube_sdk/connectycube_sdk.dart';
+
+const String APP_ID = "6560";
+const String AUTH_KEY = "d3LFWPEBSsYUaOD";
+const String AUTH_SECRET = "5K4tn6LTxwj6aG7";
+//const String ACCOUNT_ID = "TpuBZox_HPxofh7PVZdP";
+const String DEFAULT_PASS = "12345678";
+
+List<CubeUser> users = [
+  CubeUser(
+    id: 6381282,
+    login: "berkan",
+    fullName: "Berkan",
+    password: DEFAULT_PASS,
+  ),
+  CubeUser(
+    id: 6381894,
+    login: "feyza",
+    fullName: "Feyza",
+    password: DEFAULT_PASS,
+  ),
+];
